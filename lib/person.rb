db1dd67eb95b3cb2b7e329bb6744b15f2637541f
sainamercy <sainamercy@gmail.com>
2023-02-10 23:05:25 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,82 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :hygiene, :happiness
+    def initialize(name, happiness = 8)
+        @name = name 
+        @bank_account = 25
+        @happiness = happiness
+        @hygiene = 8      
+    end
+    def happiness
+        if @happiness > 10
+            10   
+        elsif @happiness < 0
+            0
+        else
+            @happiness  
+        end  
+    end 
+    def hygiene
+        if @hygiene > 10
+            10   
+        elsif @hygiene < 0
+            0
+        else
+            @hygiene  
+        end  
+    end 
+    def clean?
+        self.hygiene > 7
+    end 
+    def happy?
+        self.happiness > 7 
+    end 
+    def get_paid amount
+        self.bank_account+=amount
+        'all about the benjamins'
+    end
+    def take_bath
+        self.hygiene+=4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+    def work_out
+        self.happiness+=2
+        self.hygiene-=3
+        '♪ another one bites the dust ♫'
+    end
+    def call_friend friend
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+    def start_conversation(friend, topic)
+        @topic = topic
+        case @topic
+        when 'politics'
+            self.happiness -= 2
+            friend.happiness -= 2
+            'blah blah partisan blah lobbyist'
+        when 'weather'
+            self.happiness += 1
+            friend.happiness += 1
+            'blah blah sun blah rain'
+        else
+            'blah blah blah blah blah'
+        end
+    end
+
+end
+# mary = Person.new("Mary")
+# puts mary.name
+# mary.bank_account = 23
+# puts mary.bank_account
+# mary.happiness = 5
+# puts mary.happiness
+# mary.hygiene = 10
+# puts mary.hygiene
+# puts mary.clean?
+# puts mary.happy?
+# puts mary.get_paid(100)
+# puts mary.bank_account
+
